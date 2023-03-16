67eb5b9b58168ad05d0f8dd70755982365862ae6
Tim Ngeno <h34r54y5@gmail.com>
2023-03-16 04:36:33 -0700

    Create a C program using the function puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-/*create a program using puts function
- *end with newline
+/**
+ *main -Entry point
+ *
+ *Return: Always 0 (success)
  */
-
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
-
