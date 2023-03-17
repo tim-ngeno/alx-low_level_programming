1387748ab11abd03277a9e94089e9181efa7b176
Tim Ngeno <h34r54y5@gmail.com>
2023-03-17 03:51:43 -0700

    Print whether the last digit stored in the variable n is positive or negative

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,17 +9,17 @@
  */
 int main(void)
 {
-	int n, last_digit;
+	int n, last_n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	last_digit = n % 10;
-	if (last_digit == 0)
-		printf("Last digit of %d is %d and is 0\n", n, last_digit);
-	else if (last_digit > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
-	else if (last_digit < 6 && last_digit != 0)
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
+	last_n = n % 10;
+	if (last_n == 0)
+		printf("Last digit of %d is %d and is 0\n", n, last_n);
+	else if (last_n > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, last_n);
+	else if (last_n < 6 && last_n != 0)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_n);
 	return (0);
 }
 
