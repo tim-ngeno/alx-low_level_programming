1cfbf10814283a9a99b2639a28c42d0cb2dd51c3
Tim Ngeno <h34r54y5@gmail.com>
2023-03-21 13:24:19 -0700

    Return the absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - Print the absolute value of a number
+ * @c: the integer to be checked
+ * Return: 0 success
+ */
+int _abs(int c)
+{
+	if (c < 0)
+	{
+		c * -1;
+	}
+	return (c);
+}
