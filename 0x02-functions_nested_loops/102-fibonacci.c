fde7ddf9b317e14f17cb8782491ee458c7365d65
Tim Ngeno <h34r54y5@gmail.com>
2023-03-22 09:32:33 -0700

    Print the fibonacci sequence up to the first 50 characters

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0(success)
+ *
+ */
+int main(void)
+{
+	int n;
+	unsigned long f1 = 0, f2 = 1, sum;
+
+	for (n = 0; n < 50; n++)
+	{
+		sum = f1 + f2;
+		printf("%lu", sum);
+
+		f1 = f2;
+		f2 = sum;
+
+		if (count == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+
+	return (0);
+}
