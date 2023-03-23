5774314a1f7610e65a57bf9dfd76832bc2dc204d
Tim Ngeno <h34r54y5@gmail.com>
2023-03-23 07:55:34 -0700

    print the FizzBuzz test from 0 to 100

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,13 +12,25 @@ int main(void)
 	for (z = 1; z <= 100; z++)
 	{
 		if ((z % 3 == 0) && (z % 5 == 0))
-			printf("FizzBuzz ");
+		{
+			printf("FizzBuzz");
+			printf(" ");
+		}
 		else if (z % 5 == 0)
-			printf("Buzz ");
+		{
+			printf("Buzz");
+			printf(" ");
+		}
 		else if (z % 3 == 0)
-			printf("Fizz ");
+		{
+			printf("Fizz");
+			printf(" ");
+		}
 		else
-			printf("%d ", z);
+		{
+			printf("%d", z);
+			printf(" ");
+		}
 	}
 	printf("\n");
 }
