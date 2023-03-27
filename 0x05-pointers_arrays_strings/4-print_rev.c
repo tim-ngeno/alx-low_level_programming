5e8c3d374559b73599dbebbcca29c7fbbfae04a9
Tim Ngeno <h34r54y5@gmail.com>
2023-03-27 12:27:24 -0700

    print a string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_rev - print a string in reverse order
+ * @s: string
+ * Return: void
+ */
+void print_rev(char *s)
+{
+        int index;
+        int len;
+
+        len = _strlen(s);
+
+        for (index = len - 1; index >= 0; index--)
+        {
+                _putchar(s[index]);
+        }
+        _putchar('\n');
+}
+
+/**
+ * _strlen - Returns the length of a string
+ * @s: the string to be checked
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+        int len = 0;
+
+        while (*s != '\0')
+        {
+                ++len;
+                ++s;
+        }
+        return (len);
+}
