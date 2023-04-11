39374a503e754ec5acd110ac2643a89e7072fbd1
Tim Ngeno <h34r54y5@gmail.com>
2023-04-11 12:20:53 +0300

    add a malloc return to the function

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,8 +15,12 @@ char *create_array(unsigned int size, char c)
 	char *s;
 	unsigned int i = 0;
 
-	s = malloc((size + 1) * sizeof(char));
+	s = malloc((size) * sizeof(char));
 
+	if (s == NULL)
+	{
+		return (NULL);
+	}
 	if (size == 0)
 	{
 		return (NULL);
