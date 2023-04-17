343c32625bb4666ce5a45521750a11bf12abb36e
Tim Ngeno <h34r54y5@gmail.com>
2023-04-17 23:01:45 +0300

    write a function that creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <stddef.h>
+#include <string.h>
 
 /**
  * new_dog - creates a new dog
@@ -13,15 +13,46 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *my_dog;
+	int i, j;
+	int count_name, count_owner;
 
 	my_dog = malloc(sizeof(dog_t));
 
+	count_name = strlen(name);
 	if (my_dog == NULL)
 		return (NULL);
 
-	my_dog->name = name;
+	my_dog->name = malloc(count_name + 1);
+	if (my_dog->name == NULL)
+	{
+		free(my_dog);
+		return (NULL);
+	}
+
+
+	for (i = 0; i < count_name; i++)
+	{
+		my_dog->name[i] = name[i];
+	}
+	my_dog->name[count_name] = '\0';
+
+
+	count_owner = strlen(owner);
+	my_dog->owner = malloc(count_owner + 1);
+	if (my_dog->owner == NULL)
+	{
+		free(my_dog);
+		free(my_dog->name);
+		return (NULL);
+	}
+
+	for (j = 0; j < count_owner; j++)
+	{
+		my_dog->owner[j] = owner[j];
+	}
+
+	my_dog->owner[count_owner] = '\0';
 	my_dog->age = age;
-	my_dog->owner = owner;
 
 	return (my_dog);
 }
