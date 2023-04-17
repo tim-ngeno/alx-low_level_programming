8eb06fbe088000081ab2909e4bde169cdd811a67
Tim Ngeno <h34r54y5@gmail.com>
2023-04-17 21:46:21 +0300

    define struct dog and add function header prototypes

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,26 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog
+/**
+ * struct dog - structure for variable dog
+ * @name: variable for dog name
+ * @age: variable for dog age
+ * @owner: variable for dog owner
+ *
+ */
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
 
 #endif /* ifndef DOG_H */
