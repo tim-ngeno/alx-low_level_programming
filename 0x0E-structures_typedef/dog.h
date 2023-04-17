94245d842fd160271f513749bed20ec401c6e9fe
Tim Ngeno <h34r54y5@gmail.com>
2023-04-17 22:19:36 +0300

    update the header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,12 +8,13 @@
  * @owner: variable for dog owner
  *
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+};
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
