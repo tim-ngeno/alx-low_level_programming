70412de23174bcfe75386569c0993a81b70c5f23
Tim Ngeno <h34r54y5@gmail.com>
2023-04-17 22:30:08 +0300

    update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -19,8 +19,7 @@ void print_dog(struct dog *d)
 	else
 		printf("Name: %s\n", d->name);
 
-	if (d->age < 0)
-		printf("Age: (nil)\n");
+	printf("Age: (nil)\n");
 	else
 		printf("Age: %f\n", d->age);
 
