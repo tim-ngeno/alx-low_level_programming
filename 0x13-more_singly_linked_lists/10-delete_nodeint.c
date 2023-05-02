d700db3f719c46efa51fa0221174cfb28f3ad08c
Tim Ngeno <h34r54y5@gmail.com>
2023-05-02 23:33:26 +0300

    correct function to delete node

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -14,14 +14,14 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *previous = *head;
 	unsigned int i = 0;
 
-	if (*head == NULL)
+	if (*head == NULL || head == NULL)
 		return (-1);
 
 	else if (index == 0)
 	{
 		*head = (*head)->next;
 		free(current);
-		current = NULL;
+
 	}
 
 	else
@@ -34,7 +34,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		}
 		previous->next = current->next;
 		free(current);
-		current = NULL;
 	}
+
 	return (1);
 }
