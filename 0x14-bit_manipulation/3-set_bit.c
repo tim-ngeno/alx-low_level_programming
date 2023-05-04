11dc9a34e5d73242097097647da834d54584b254
Tim Ngeno <h34r54y5@gmail.com>
2023-05-04 23:20:45 +0300

    set the value of a bit to 1 - update

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -12,7 +12,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned int t;
 
-	if (n == NULL)
+	if (n == NULL || (index >= (sizeof(unsigned long int) * 8)))
 		return (-1);
 
 	t = 1 << index;
