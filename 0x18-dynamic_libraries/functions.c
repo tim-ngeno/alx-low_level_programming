5bf5cc88226f6cd34a7105cbee08b543c72a5017
Tim Ngeno <h34r54y5@gmail.com>
2023-06-19 12:15:04 +0300

    add functions to be called in shared lib

---
+++ 0x18-dynamic_libraries/functions.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+/**
+ * add - adds two integers a and b
+ * @a: the first input integer
+ * @b: the second input integer
+ * Return: the result of the addition
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtracts two integers, a from b
+ * @a: first input integer
+ * @b: second input integer
+ * Return: result of subtraction
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies two integers a and b
+ * @a: first input integer
+ * @b: second input integer
+ * Return: product of the two integers
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides two integers, a by b
+ * @a: first input integer
+ * @b: the second integer
+ * Return: the division result
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+
+/**
+ * mod - returns the remainder of a division
+ * @a: first input integer
+ * @b: second input integer
+ * Return: modulus
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
