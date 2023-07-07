900a5780e756dfdb7fb03f4579c80a799c904fe3
Tim Ngeno <h34r54y5@gmail.com>
2023-07-07 10:23:55 +0300

    retrieve a value associated with a key - update

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -13,6 +13,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned int index;
 	hash_node_t *entry;
 
+	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
+	    key == NULL || strlen(key) == 0)
+		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
 	entry = ht->array[index];
 
