3c18cdbfc5988c8f424f0bf6530d75cb00cab441
Tim Ngeno <h34r54y5@gmail.com>
2023-07-08 10:33:19 +0300

    fix spaces

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -22,7 +22,7 @@ void hash_table_print(const hash_table_t *ht)
 		{
 			if (t == 1)
 				printf(", ");
-			printf("'%s' : '%s'", temp->key, temp->value);
+			printf("'%s': '%s'", temp->key, temp->value);
 			t = 1;
 			temp = temp->next;
 		}
