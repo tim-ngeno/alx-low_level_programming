f040ce3da7a994e350f33b6ac02e8f0489edf374
Tim Ngeno <timngeno424@gmail.com>
2023-10-10 08:58:08 +0300

    add function definition prototypes

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,14 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+/* Standard Libraries */
+#include <stddef.h>
+#include <stdio.h>
+
+/* Function prototypes */
+
+int linear_search(int *array, size_t size, int value);
+
+int binary_search(int *array, size_t size, int value);
+
+#endif	/* SEARCH_ALGOS_H */
